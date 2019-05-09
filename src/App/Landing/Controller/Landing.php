b2172eb1681176d4fd0986852a3be58f8223be69
Saitamchik <Savvaak47@mail.ru>
2019-05-09 21:30:21 +0700

    Вывод из бд

--- src/App/Landing/Controller/Landing.php
+++ src/App/Landing/Controller/Landing.php
@@ -6,6 +6,10 @@ use Base\Render\RendererInterface;
 use Verse\Di\Env;
 use Verse\Run\Controller\SimpleController;
 
+    $mass = "Mass.json";
+    $content = file_get_contents($mass);
+    $content =  json_decode($content, true);
+    $id = 1;
 class Landing extends SimpleController
 {
     /**
@@ -23,23 +27,37 @@ class Landing extends SimpleController
 
     public function index () 
     {
+            $tmp ="Тест";
+            $mass = "Mass.json";
+            $content = file_get_contents($mass);
+            $content =  json_decode($content, true);
+            $id = 1;
+
+    foreach($content as $header => $value) {
         return $this->_render(__FUNCTION__, [
-            'title' => 'Main Page',
+            'ma' => $content ,
+            'header' => $header ,
+            'content' => $value ,
         ]);
+      };
     }
+
     public function team () 
     {
         return $this->_render(__FUNCTION__, [
             'title' => 'team',
         ]);
     }
+
     public function contacts () 
     {
         return $this->_render(__FUNCTION__, [
             'title' => 'Contacts Page',
         ]);
+
     }
-    
+
+
     private function _render($template, $data = []) {
         return $this->_renderer->render($template, $data,
             'page',
