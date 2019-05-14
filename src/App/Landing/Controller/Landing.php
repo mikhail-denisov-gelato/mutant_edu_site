38c7263ce9770f6a161eb757812da8bfc72db097
Saitamchik <Savvaak47@mail.ru>
2019-05-14 23:13:50 +0700

    Попытка реализовать авторизацию

--- src/App/Landing/Controller/Landing.php
+++ src/App/Landing/Controller/Landing.php
@@ -6,10 +6,50 @@ use Base\Render\RendererInterface;
 use Verse\Di\Env;
 use Verse\Run\Controller\SimpleController;
 
+
     $mass = "Mass.json";
     $content = file_get_contents($mass);
     $content =  json_decode($content, true);
     $id = 1;
+/*
+class Auth
+{
+    private $authPairs = [
+        'duane' => 'DuaneTheBest!!!1',
+        'mike' => 'Mikeismike22'
+    ];
+    private $authKeys = [
+        'duane' => 'MCka&(!jlV_*76AUH_njfvb78asd',
+        'mike' => 'nMCYUW>KVB)0!VAgcaiAFSHCJB'
+    ];
+    public function checkLoginAndPassAndReturnKey($login, $pass)
+    {
+        $storedPass = $this->authPairs[$login] ?? null;
+        if (!$storedPass) {
+            return false;
+        }
+        if ($storedPass !== $pass) {
+            return false;
+        }
+        $key = $this->authKeys[$login];
+        if (!$key) {
+            return false;
+        }
+        return $key;
+    }
+    public function checkAuthKey($login, $key)
+    {
+        $storedPass = $this->authKeys[$login] ?? null;
+        if (!$storedPass) {
+            return false;
+        }
+        if ($storedPass !== $key) {
+            return false;
+        }
+        return true;
+    }
+}
+*/
 class Landing extends SimpleController
 {
     /**
@@ -46,15 +86,29 @@ class Landing extends SimpleController
             'title' => 'team',
         ]);
     }
-
+/*
     public function contacts () 
     {
+        $login =  $this- > > p('login');
+        $pass =  $this- > > p('pass');
+
+        $auth =  new Auth();
+        $success = false;
+
+        if($key =  $auth- > > checkLoginAndPassAndReturnKey($login, $pass)) {
+            $this- > > requestWrapper- > > setState('key', $key);
+            $this- > > requestWrapper- > > setState('user_id', $login);
+            $success = true;
+        }
         return $this->_render(__FUNCTION__, [
             'title' => 'Contacts Page',
         ]);
-
+        return $this- > > _render('contacts', [
+            'success'  = > >  $success,
+            'login'    = > >  $login,
+        ]);
     }
-
+*/
 
     private function _render($template, $data = []) {
         return $this->_renderer->render($template, $data,
